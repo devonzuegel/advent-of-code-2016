2912539e3746880e8c6f40e057ae59eea6dd6713
devon <devon@affirm.com>
2016-12-11 13:56:21 -0800

    Add types to day 2 problem

--- 2__bathroom_security.ts
+++ 2__bathroom_security.ts
@@ -1,12 +1,5 @@
 import * as R from 'ramda'
 
-export const EXAMPLE_INPUT = [
-  'ULL',
-  'RRDDD',
-  'LURDL',
-  'UUUUD',
-]
-
 const charToDirection = (char: string) => {
   switch (char) {
     case 'U':  return { x: 0,  y: 1  }
@@ -17,13 +10,25 @@ const charToDirection = (char: string) => {
   }
 }
 
-export const getSteps = input =>
+export const EXAMPLE_INPUT = [
+  'ULL',
+  'RRDDD',
+  'LURDL',
+  'UUUUD',
+]
+
+export interface Location {
+  x: number,
+  y: number,
+}
+
+export const getSteps = (input: string[]): string[] =>
   input.reduce(
     (soFar, step) => soFar.concat(step.split('')),
     []
    )
 
-export const getLocations = (steps, initialLoc, onBoard) =>
+export const getLocations = (steps: string[], initialLoc: Location, onBoard: Function): Location[] =>
   steps.reduce(
     (soFar, char) => {
       const currLoc   = soFar[soFar.length - 1]
@@ -37,14 +42,14 @@ export const getLocations = (steps, initialLoc, onBoard) =>
     [initialLoc]
   )
 
-export const getDigitIndices = input =>
+export const getDigitIndices = (input: string[]): number[] =>
   input.reduce(
     (soFar, line) => [...soFar, (soFar[soFar.length - 1] || 0) + line.length],
     []
    )
 
-export const instructionsToPassword = (input, coordinateToNum, initialLoc, onBoard) => {
-  const locations = getLocations(getSteps(input), initialLoc, onBoard)
-  const digits = getDigitIndices(input).map(i => coordinateToNum(locations[i]))
-  return digits.join('')
+export const instructionsToPassword = (input: string[], coordinateToNum: Function, initialLoc: Location, onBoard: Function): string => {
+  const locations     = getLocations(getSteps(input), initialLoc, onBoard)
+  const passwordChars = getDigitIndices(input).map(i => coordinateToNum(locations[i]))
+  return passwordChars.join('')
 }
