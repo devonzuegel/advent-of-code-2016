267868b3298772ad4043707a578ee9de95640d91
devon <devon@affirm.com>
2016-12-11 13:18:17 -0800

    Solve day 2 challenge, part 2

--- 2__bathroom_security.ts
+++ 2__bathroom_security.ts
@@ -1,19 +1,10 @@
-import { cmp, p } from './test_utils'
 import * as R from 'ramda'
 
-const EXAMPLE_INPUT = [
+export const EXAMPLE_INPUT = [
   'ULL',
   'RRDDD',
   'LURDL',
   'UUUUD',
-]
-
-const CHALLENGE_INPUT = [
-  'LRRLLLRDRURUDLRDDURULRULLDLRRLRLDULUDDDDLLRRLDUUDULDRURRLDULRRULDLRDUDLRLLLULDUURRRRURURULURRULRURDLULURDRDURDRLRRUUDRULLLLLDRULDDLLRDLURRLDUURDLRLUDLDUDLURLRLDRLUDUULRRRUUULLRDURUDRUDRDRLLDLDDDLDLRRULDUUDULRUDDRLLURDDRLDDUDLLLLULRDDUDDUUULRULUULRLLDULUDLLLLURRLDLUDLDDLDRLRRDRDUDDDLLLLLRRLLRLUDLULLDLDDRRUDDRLRDDURRDULLLURLRDLRRLRDLDURLDDULLLDRRURDULUDUDLLLDDDLLRLDDDLLRRLLURUULULDDDUDULUUURRUUDLDULULDRDDLURURDLDLULDUDUDDDDD',
-  'RUURUDRDUULRDDLRLLLULLDDUDRDURDLRUULLLLUDUDRRUDUULRRUUDDURDDDLLLLRRUURULULLUDDLRDUDULRURRDRDLDLDUULUULUDDLUDRLULRUDRDDDLRRUUDRRLULUULDULDDLRLURDRLURRRRULDDRLDLLLRULLDURRLUDULDRDUDRLRLULRURDDRLUDLRURDDRDULUDLDLLLDRLRUDLLLLLDUDRDUURUDDUDLDLDUDLLDLRRDLULLURLDDUDDRDUDLDDUULDRLURRDLDLLUUDLDLURRLDRDDLLDLRLULUDRDLLLDRLRLLLDRUULUDLLURDLLUURUDURDDRDRDDUDDRRLLUULRRDRULRURRULLDDDUDULDDRULRLDURLUDULDLDDDLRULLULULUDLDDRDLRDRDLDULRRLRLRLLLLLDDDRDDULRDULRRLDLUDDDDLUDRLLDLURDLRDLDRDRDURRDUDULLLDLUDLDRLRRDDDRRLRLLULDRLRLLLLDUUURDLLULLUDDRLULRDLDLDURRRUURDUDRDLLLLLLDDDURLDULDRLLDUDRULRRDLDUDRLLUUUDULURRUR',
-  'URRRLRLLDDDRRLDLDLUDRDRDLDUDDDLDRRDRLDULRRDRRDUDRRUUDUUUDLLUURLRDRRURRRRUDRLLLLRRDULRDDRUDLRLUDURRLRLDDRRLUULURLURURUDRULDUUDLULUURRRDDLRDLUDRDLDDDLRUDURRLLRDDRDRLRLLRLRUUDRRLDLUDRURUULDUURDRUULDLLDRDLRDUUDLRLRRLUDRRUULRDDRDLDDULRRRURLRDDRLLLRDRLURDLDRUULDRRRLURURUUUULULRURULRLDDDDLULRLRULDUDDULRUULRRRRRLRLRUDDURLDRRDDULLUULLDLUDDDUURLRRLDULUUDDULDDUULLLRUDLLLRDDDLUUURLDUDRLLLDRRLDDLUDLLDLRRRLDDRUULULUURDDLUR',
-  'UULDRLUULURDRLDULURLUDULDRRDULULUDLLDURRRURDRLRLLRLDDLURRDLUUDLULRDULDRDLULULULDDLURULLULUDDRRULULULRDULRUURRRUDLRLURDRURDRRUDLDDUURDUUDLULDUDDLUUURURLRRDLULURDURRRURURDUURDRRURRDDULRULRRDRRDRUUUUULRLUUUDUUULLRRDRDULRDDULDRRULRLDLLULUUULUUDRDUUUDLLULDDRRDULUURRDUULLUUDRLLDUDLLLURURLUDDLRURRDRLDDURLDLLUURLDUURULLLRURURLULLLUURUUULLDLRDLUDDRRDDUUDLRURDDDRURUURURRRDLUDRLUULDUDLRUUDRLDRRDLDLDLRUDDDDRRDLDDDLLDLULLRUDDUDDDLDDUURLDUDLRDRURULDULULUDRRDLLRURDULDDRRDLUURUUULULRURDUUDLULLURUDDRLDDUDURRDURRUURLDLLDDUUDLLUURDRULLRRUUURRLLDRRDLURRURDULDDDDRDD',
-  'LLRUDRUUDUDLRDRDRRLRDRRUDRDURURRLDDDDLRDURDLRRUDRLLRDDUULRULURRRLRULDUURLRURLRLDUDLLDULULDUUURLRURUDDDDRDDLLURDLDRRUDRLDULLRULULLRURRLLURDLLLRRRRDRULRUDUDUDULUURUUURDDLDRDRUUURLDRULDUDULRLRLULLDURRRRURRRDRULULUDLULDDRLRRULLDURUDDUULRUUURDRRLULRRDLDUDURUUUUUURRUUULURDUUDLLUURDLULUDDLUUULLDURLDRRDDLRRRDRLLDRRLUDRLLLDRUULDUDRDDRDRRRLUDUDRRRLDRLRURDLRULRDUUDRRLLRLUUUUURRURLURDRRUURDRRLULUDULRLLURDLLULDDDLRDULLLUDRLURDDLRURLLRDRDULULDDRDDLDDRUUURDUUUDURRLRDUDLRRLRRRDUULDRDUDRLDLRULDL',
 ]
 
 const charToDirection = (char: string) => {
@@ -26,95 +17,34 @@ const charToDirection = (char: string) => {
   }
 }
 
-const getSteps = input =>
+export const getSteps = input =>
   input.reduce(
     (soFar, step) => soFar.concat(step.split('')),
     []
    )
 
-const onBoard = val => Math.abs(val) <= 1
-
-const getLocations = steps =>
+export const getLocations = (steps, initialLoc, onBoard) =>
   steps.reduce(
     (soFar, char) => {
       const currLoc   = soFar[soFar.length - 1]
       const direction = charToDirection(char)
       const newLoc    = {
-        x: currLoc.x + (onBoard(currLoc.x + direction.x) ? direction.x : 0),
-        y: currLoc.y + (onBoard(currLoc.y + direction.y) ? direction.y : 0),
+        x: currLoc.x + direction.x,
+        y: currLoc.y + direction.y,
       }
-      return soFar.concat([newLoc])
+      return soFar.concat([onBoard(newLoc) ? newLoc : currLoc])
     },
-    [{ x: 0, y: 0 }]
+    [initialLoc]
   )
 
-const coordinateToNum = ({ x, y }) =>
-  ((x + 1) % 3) + ((1 - y) * 3) + 1
-
-const getDigitIndices = input =>
+export const getDigitIndices = input =>
   input.reduce(
     (soFar, line) => [...soFar, (soFar[soFar.length - 1] || 0) + line.length],
     []
    )
 
-const instructionsToPassword = input => {
-  const locations = getLocations(getSteps(input))
+export const instructionsToPassword = (input, coordinateToNum, initialLoc, onBoard) => {
+  const locations = getLocations(getSteps(input), initialLoc, onBoard)
   const digits = getDigitIndices(input).map(i => coordinateToNum(locations[i]))
   return digits.join('')
 }
-
-const TESTS = [
-  [ coordinateToNum({ x: -1,  y:  1 }), 1 ],
-  [ coordinateToNum({ x:  0,  y:  1 }), 2 ],
-  [ coordinateToNum({ x:  1,  y:  1 }), 3 ],
-  [ coordinateToNum({ x: -1,  y:  0 }), 4 ],
-  [ coordinateToNum({ x:  0,  y:  0 }), 5 ],
-  [ coordinateToNum({ x:  1,  y:  0 }), 6 ],
-  [ coordinateToNum({ x: -1,  y: -1 }), 7 ],
-  [ coordinateToNum({ x:  0,  y: -1 }), 8 ],
-  [ coordinateToNum({ x:  1,  y: -1 }), 9 ],
-  [
-    getSteps(EXAMPLE_INPUT),
-    ['U', 'L', 'L', 'R', 'R', 'D', 'D', 'D', 'L', 'U', 'R', 'D', 'L', 'U', 'U', 'U', 'U', 'D']
-  ], [
-    getLocations(['U', 'U', 'U']),
-    [
-      { x: 0, y: 0 },
-      { x: 0, y: 1 },
-      { x: 0, y: 1 },
-      { x: 0, y: 1 },
-    ]
-  ], [
-    getLocations(['U', 'R', 'D', 'L']),
-    [
-      { x: 0, y: 0 },
-      { x: 0, y: 1 },
-      { x: 1, y: 1 },
-      { x: 1, y: 0 },
-      { x: 0, y: 0 },
-     ]
-  ], [
-    getLocations(['U', 'R', 'D', 'L']).map(coordinateToNum),
-    [ 5, 2, 3, 6, 5 ]
-  ], [
-    getLocations(['U', 'U', 'U', 'U']).map(coordinateToNum),
-    [ 5, 2, 2, 2, 2 ]
-  ], [
-    getDigitIndices(EXAMPLE_INPUT),
-    [3, 8, 13, 18]
-  ], [
-    getDigitIndices(['x', 'x', 'x', 'x']),
-    [1, 2, 3, 4]
-  ], [
-    getDigitIndices(['xx', 'xx', 'xx', 'xx']),
-    [2, 4, 6, 8]
-  ], [
-    instructionsToPassword(EXAMPLE_INPUT),
-    "1985",
-  ], [
-    instructionsToPassword(CHALLENGE_INPUT),
-    "73597",
-  ],
-]
-
-TESTS.map(test => cmp(test[0], test[1]))
